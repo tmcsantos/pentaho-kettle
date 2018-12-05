5e742d3043ee550868b9db8aef34feb0031981fc
Tiago Santos <tiago.dos.santos@hitachivantara.com>
2018-12-05 18:21:14 +0000

    test

--- engine/src/main/java/org/pentaho/di/base/AbstractBaseCommandExecutor.java
+++ engine/src/main/java/org/pentaho/di/base/AbstractBaseCommandExecutor.java
@@ -45,6 +45,7 @@ import org.pentaho.metastore.stores.delegate.DelegatingMetaStore;
 import org.pentaho.di.version.BuildVersion;
 
 public abstract class AbstractBaseCommandExecutor {
+  private boolean test = false;
 
   private SimpleDateFormat dateFormat = new SimpleDateFormat( "yyyy/MM/dd HH:mm:ss.SSS" );
 
